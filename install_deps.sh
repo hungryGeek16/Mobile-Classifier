5f799e83ef4cdb3a998a10393f9332aa064bd7cb
Rahul Mangalampalli <rahul_mangalampalli@yahoo.in>
2021-10-01 15:26:41 +0530

    dependencies installation script

---
+++ install_deps.sh
@@ -0,0 +1,12 @@
+sudo add-apt-repository ppa:lkoppel/opencv
+sudo apt-get -y update
+sudo apt install -y libopencv-calib3d3.2 libopencv-core3.2 libopencv-features2d3.2  libopencv-flann3.2 libopencv-highgui3.2 libopencv-imgcodecs3.2 libopencv-imgproc3.2 libopencv-ml3.2 libopencv-objdetect3.2 libopencv-photo3.2 libopencv-shape3.2 libopencv-stitching3.2 libopencv-superres3.2 libopencv-video3.2 libopencv-videoio3.2 libopencv-videostab3.2 libopencv-viz3.2 libopencv3.2-jni libopencv3.2-java libopencv-contrib3.2 #OpenCV deps
+sudo apt install -y libopencv-calib3d-dev libopencv-core-dev libopencv-features2d-dev  libopencv-flann-dev libopencv-highgui-dev libopencv-imgcodecs-dev libopencv-imgproc-dev libopencv-ml-dev libopencv-objdetect-dev libopencv-photo-dev libopencv-shape-dev libopencv-stitching-dev libopencv-superres-dev libopencv-video-dev libopencv-videoio-dev libopencv-videostab-dev libopencv-viz-dev libopencv-contrib-dev libopencv-dev #OpenCV deps
+sudo apt-get install -y python3-opencv # OpenCV deps
+sudo apt-get install -y libopenblas-dev # Math Library
+sudo apt-get install -y libboost-all-dev # Math Library
+sudo apt install -y python3-pip
+pip3 install protobuf==3.11.3
+sudo apt-get install -y libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev # Storage formats 
+sudo apt-get install -y python3-dev 
+sudo apt-get install -y libgflags-dev libgoogle-glog-dev liblmdb-dev # Flag libraries
